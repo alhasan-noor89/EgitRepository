1d591f39f4a02ba546be10fbd1fd9ee491b5820e
alhasan-noor89 <noor@noor.LEADER.COM>
2013-08-19 11:14:16 +0200

    third commit by noor

--- EGithub/src/com/example/egithub/MainActivity.java
+++ EGithub/src/com/example/egithub/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Toast.makeText(this, "This is the first project version",
+		Toast.makeText(this, "This is the third project version by noor",
 				Toast.LENGTH_LONG).show();
 	}
 
