8934831c5addd0db111c1ad138e8f9bdad801d74
alhasan-noor89 <alhasan.noor@hotmail.com>
2013-08-19 14:01:04 +0200

    Update MainActivity.java

--- EGithub/src/com/example/egithub/MainActivity.java
+++ EGithub/src/com/example/egithub/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyttttttttttttttttttttttt
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxytttttttttttttttttttttttby noor alhassan
 		Toast.makeText(this, "This is the 6 project version by noor",
 				Toast.LENGTH_LONG).show();
 	}
